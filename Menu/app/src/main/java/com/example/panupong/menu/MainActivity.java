da5fcc26bc2f8a36bf297ed2f82e286c4affc39e
PanupongDeve <5635512110@psu.ac.th>
2018-05-03 21:47:11 +0700

    add Recycle view

--- Menu/app/src/main/java/com/example/panupong/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/panupong/menu/MainActivity.java
@@ -1,100 +1,40 @@
 package com.example.panupong.menu;
 
-import android.content.res.Configuration;
-import android.support.annotation.Nullable;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
-    TextView tvOutput;
-    ActionBarDrawerToggle actionBarDrawerToggle;
-    DrawerLayout mDrawerLayout;
-    Tools tools = new Tools();
-    private String[] mDrawerTitle = {"File", "Load...", "Open", "Contact"};
-    private ListView mListView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvOutput = (TextView) findViewById(R.id.tvOutput);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
-                mDrawerLayout,
-                R.string.open_drawer,
-                R.string.close_drawer);
-        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        int[] resId = {R.drawable.ic_action_alarm,
+                R.drawable.ic_action_github,
+                R.drawable.ic_action_google_play};
 
-        // Set ListView
+        String[] list = {"Alarm", "GitHub", "GooglePlay"};
 
-        mListView = (ListView) findViewById(R.id.drawer);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, mDrawerTitle);
-        mListView.setAdapter(adapter);
+        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), list, resId);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String itemValue = (String) mListView.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), "Position :"+ position+ " ListItem : " +itemValue ,
-                        Toast.LENGTH_SHORT).show();
-
+        ListView listView = (ListView)findViewById(R.id.listView1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long row_id) {
+                TextView textView = (TextView)view.findViewById(R.id.textView1);
+                System.out.println( textView.getText());
+                Toast.makeText(getApplicationContext(), "Clicked on item:" +  position , Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item))
-            return true;
-        switch (item.getItemId()) {
-            case R.id.mnuNew:
-                tvOutput.setText("Hello New!");
-                tools.todo("TODO Success").todo("Callback Success");
-                Toast.makeText(this, "New!", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.mnuHelp:
-                tvOutput.setText("Hello Help!");
-                Toast.makeText(this, "Help!", Toast.LENGTH_SHORT).show();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        actionBarDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        actionBarDrawerToggle.syncState();
-    }
 }
+
+
+
+
