910c50a5585c84924509423c3b9b57dc9877567e
PanupongDeve <5635512110@psu.ac.th>
2018-05-03 18:22:58 +0700

    learn menu

---
+++ Menu/app/src/main/java/com/example/panupong/menu/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.panupong.menu;
+
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvOutput;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout mDrawerLayout;
+    Tools tools = new Tools();
+    private String[] mDrawerTitle = {"File", "Load...", "Open", "Contact"};
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOutput = (TextView) findViewById(R.id.tvOutput);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
+                mDrawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer);
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Set ListView
+
+        mListView = (ListView) findViewById(R.id.drawer);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mDrawerTitle);
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        switch (item.getItemId()) {
+            case R.id.mnuNew:
+                tvOutput.setText("Hello New!");
+                tools.todo("TODO Success").todo("Callback Success");
+                Toast.makeText(this, "New!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.mnuHelp:
+                tvOutput.setText("Hello Help!");
+                Toast.makeText(this, "Help!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+}
---
+++ Menu/app/src/main/java/com/example/panupong/menu/Tools.java
@@ -0,0 +1,17 @@
+package com.example.panupong.menu;
+
+public class Tools {
+    public Tools todo(String message1) {
+        System.out.println(message1);
+        return this;
+    };
+
+    public void callback(String message2){
+        System.out.println(message2);
+    };
+
+}
+
+
+
+
